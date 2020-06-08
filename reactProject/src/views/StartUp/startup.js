e39d861ffca0c7179f3c219ad4eb37f64c009d93
coldzlz321 <626736944@qq.com>
2020-06-08 20:47:26 +0800

    change

---
+++ reactProject/src/views/StartUp/startup.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+
+import TimerMixin from 'react-timer-mixin';
+
+export default class StartUpPage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentWillMount(){
+        
+    }
+}
